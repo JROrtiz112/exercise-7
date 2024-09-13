2def3bfb2ea7828a8f1d938dfac0ce56a5869940
JROrtiz112 <jrobertoortiz112@gmail.com>
2024-09-13 15:59:10 -0600

    spaceship 2 added

--- exercise7.py
+++ exercise7.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, abort
 from const import systems
 import json
 import random
@@ -6,6 +6,10 @@ import random
 app = Flask(__name__)
 system_index = -1
 item_picked = {}
+
+CRITICAL_PRESSURE = 10
+CRITICAL_SPECIFIC_VOLUME = 0.0035
+MIN_TEMPERATURE = 30
 
 @app.route("/status", methods=['GET'])
 def status():
@@ -36,3 +40,28 @@ def repairBay():
 @app.route("/teapot", methods=['POST'])
 def teapot():
     return Response(status=418)
+
+@app.route("/phase-change-diagram", methods=['GET'])
+def phaseChangeDiagram():
+    mPa = request.args.get('pressure', type=float)
+    temperature = request.args.get('temperature', type=float, default=None)
+    volume = request.args.get('volume', type=float, default=None)
+
+    if (temperature is None or temperature > MIN_TEMPERATURE) and volume is None:
+        if mPa is None:
+            return abort(400, description="Pressure value is expected")
+        elif mPa < CRITICAL_PRESSURE:
+            return abort(400, description="Pressure is lower than expected")
+        elif mPa > CRITICAL_PRESSURE:
+            return abort(400, description="Pressure is higher than expected")
+        else:
+            data = { "specific_volume_liquid": CRITICAL_SPECIFIC_VOLUME, "specific_volume_vapor": CRITICAL_SPECIFIC_VOLUME }
+            return json.dumps(data)
+        
+    elif temperature < MIN_TEMPERATURE:
+        return abort(400, description="Temperature is either below than expected or not known")
+    else:
+        if volume < 0.00105 and volume > 30.00:
+            return abort(400, description="Volume should be between 0.00105 and 30.00 m^3/kg")
+        else:
+            return abort(400, description="Params such as pressure are expected")
